424760b62ca1e26e4c2bfe2457d98c1c2751ff18
rizzu <mdrizwanahmad078@gmail.com>
2022-10-30 00:49:20 +0530

    first commit

---
+++ increment and decrement/src/Main.java
@@ -0,0 +1,14 @@
+class prepost
+        {
+            public static void main(String[] args) {
+                int a=10;
+        System.out.println(a);
+        System.out.println("prepost Incr..");
+        System.out.println(a++);
+        System.out.println(++a);
+
+                System.out.println("prepost decr..");
+                System.out.println(a--);
+                System.out.println(--a);
+    }
+}
